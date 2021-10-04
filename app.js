5f7f51827e7d9ae3263c7fb6c9482712d3942b69
abdullahalshawafi <abdullahadel.aam@gmail.com>
2021-10-05 00:35:52 +0200

    Initial project setup

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+const DB_URI = process.env.DB_URI || 'mongodb://localhost:27017/Bank-System';
+
+async function db(URI) {
+    await mongoose.connect(URI);
+    console.log('Connected to DB');
+}
+
+db(DB_URI).catch(err => console.log(err));
+
+app.listen(PORT, err => {
+    if (err) return console.error(err);
+    console.log(`Server started listening at port ${PORT}`);
+});
